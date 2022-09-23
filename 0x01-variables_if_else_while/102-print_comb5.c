8cc6ebba808127b07b9e224a8e0bed1b720de91a
Oluwaseun Ogundele <osgun4christ@gmail.com>
2022-09-23 23:10:14 +0000

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 		{
 			for (t = tens; t <= '9'; t++)
 			{
-				for (o = ones + 1; o <= '9'; o++)
+				for (o = (ones + 1); o <= '9'; o++)
 				{
 					putchar(tens);
 					putchar(ones);
