b0356c818907bb7ba8f10020793e500f2b655ede
Oluwaseun Ogundele <osgun4christ@gmail.com>
2022-09-23 13:47:25 +0000

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - print lower and uppercase a-zA-Z using putchar
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
+
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
