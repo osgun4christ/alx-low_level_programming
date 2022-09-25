12ca9cfca16a0232d3613013487566a50a8d7c9c
Oluwaseun Ogundele <osgun4christ@gmail.com>
2022-09-25 23:56:35 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ *main - Prints _putchar
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar('_putchar');
+	putchar('\n');
+
+	return ('0');
+}
