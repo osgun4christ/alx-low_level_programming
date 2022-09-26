0dae2db7c578e2120b7ddddfffbde935ab2f6440
Oluwaseun Ogundele <osgun4christ@gmail.com>
2022-09-26 02:49:04 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int times;
 	char letter;
 
-	for (times = 0; times <= 9; time++)
+	for (times = 0; times <= 9; times++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
