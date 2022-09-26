4bcf5f8c75c7224fa291c3e093b2c03f1e1c190d
Oluwaseun Ogundele <osgun4christ@gmail.com>
2022-09-26 02:17:33 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints 10 times the alphabet,
+ *in lowercase, followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+	int times = 0;
+	char letter;
+
+	while (times <= 9)
+	{
+		letter = 'a'
+			while (letter <= 'z')
+			{
+				_putchar(letter);
+			}
+		++letter
+	}
+	++times;
+}
