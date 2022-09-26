5f6ec40d6893e7baca46d966f8cc0ebaf9cc4850
Oluwaseun Ogundele <osgun4christ@gmail.com>
2022-09-26 01:48:56 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - Prints  prints the alphabet, in lowercase
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		++letter;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
